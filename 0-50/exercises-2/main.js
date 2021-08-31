a1e3e85242be41e6c8307664c1adeb0db1b1600e
Uanderson Nunes <uandersonmbc@hotmail.com>
2021-08-31 20:40:07 -0300

    Initial commit

---
+++ 0-50/exercises-2/main.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8");
+
+let first = parseInt(input.split(" ")[0])
+let second = parseInt(input.split(" ")[1])
+let third = parseInt(input.split(" ")[2])
